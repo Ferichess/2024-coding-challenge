c407f297d2cffee4413273b5e16396c6b3a237ca
feri sandria <feri.chess@gmail.com>
2024-02-12 11:48:44 +0700

    day 3/336

---
+++ 3-hero.js
@@ -0,0 +1,32 @@
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+// wpu coding challenge
+// 3/336
+
+// function hero(bullets, dragons) {
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function hero(bullets, dragons){
+//     return bullets >= dragons * 2
+//   }
+
+// function hero(bullets, dragons) {
+//   return bullets / 2 >= dragons ? true : false;
+// }
+
+// const hero = (bullets, dragons) => dragons * 2 <= bullets;
+
+// const hero = (bullets, dragons) => bullets / dragons >= 2
+
+// const BULLETS_PER_DRAGON = 2;
+
+// function hero(bullets, dragons) {
+//   const survive = bullets >= dragons * BULLETS_PER_DRAGON;
+//   return survive;
+// }
+
+// console.log(hero(10, 5));
