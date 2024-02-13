bac9a8c0f6f751f0bfffb537fdd0f91b55197d3a
feri sandria <feri.chess@gmail.com>
2024-02-13 15:58:33 +0700

    day 5/336

---
+++ 5-zeroFuel.js
@@ -0,0 +1,30 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+// wpu coding challenge
+// 5/336
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     if (distanceToPump <= mpg * fuelLeft) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return distanceToPump / mpg <= fuelLeft;
+// }
+
+// function zeroFuel(distanceToPump, mpg, fuelLeft) {
+//     return (distanceToPump > mpg * fuelLeft) ? false : true;
+//   }
+
+// function zeroFuel(distanceToPump, mpg, fuelLeft) {
+//     var capacity = fuelLeft * mpg;
+   
+//    return capacity >= distanceToPump;
+//    }
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft ? true : false
+
+// console.log(zeroFuel(50, 25, 2))
+// console.log(zeroFuel(100, 30, 1))
